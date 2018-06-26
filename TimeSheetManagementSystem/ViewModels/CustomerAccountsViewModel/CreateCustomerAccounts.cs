ee459b13363810d737bf6676a6d7cb9213f3545f
Zamiel Chia <zchia0801@gmail.com>
2018-06-26 23:39:28 +0800

    add some viewmodels

---
+++ TimeSheetManagementSystem/ViewModels/CustomerAccountsViewModel/CreateCustomerAccounts.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TimeSheetManagementSystem.ViewModels.CustomerAccountsViewModel
+{
+    public class CreateCustomerAccounts
+    {
+        public string AccountName { get; set; }
+        public DateTime EffectiveStartDate { get; set; }
+        public DateTime? EffectiveEndDate { get; set; }
+        public decimal RatePerHour { get; set; }
+        public string Comments { get; set; }
+        public bool isVisible { get; set; }
+        public string createdBy { get; set; }
+        public string updatedBy { get; set; }
+    }
+}
---
+++ TimeSheetManagementSystem/ViewModels/CustomerAccountsViewModel/IndexCustomerAccounts.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TimeSheetManagementSystem.ViewModels.CustomerAccountsViewModel
+{
+    public class IndexCustomerAccounts
+    {
+        // Used a viewmodel so that my customer account view can be binded to two models which is AccountRate and Customer Account.
+        public int customerAccountId { get; set; }
+        public string accountName { get; set; }
+        public string Comments { get; set; }
+        public bool isVisible { get; set; }
+        public string createdBy { get; set; }
+        public string updatedBy { get; set; }
+    }
+}
