8f4659b525f924d1fde0e99f62e858c2ea806a55
Zamiel Chia <zchia0801@gmail.com>
2018-07-24 11:22:40 +0800

    dkwat imdoing

--- TimeSheetManagementSystem/Controllers/AccountDetailsController.cs
+++ TimeSheetManagementSystem/Controllers/AccountDetailsController.cs
@@ -3,18 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TimeSheetManagementSystem.Data;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TimeSheetManagementSystem.Models;
+using ReflectionIT.Mvc.Paging;
+using Microsoft.AspNetCore.Identity;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace TimeSheetManagementSystem.Controllers
 {
+    [Route("api/[controller]")]
     public class AccountDetailsController : Controller
     {
+        private readonly ApplicationDbContext _context;
 
-        // GET: /<controller>/
-        public IActionResult Index()
+        public AccountDetailsController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
+        }
+        // GET: /<controller>/
+        public async Task<IActionResult> Index(int id, int page = 1)
+        {
+            IOrderedQueryable<AccountDetail> test = _context.AccountDetails.Where(a => a.CustomerAccountId == id).OrderBy(x => x.AccountDetailId).Select(acc => new AccountDetail
+            {
+                IsVisible = acc.IsVisible,
+                EffectiveStartDate = acc.EffectiveStartDate,
+                EffectiveEndDate = acc.EffectiveEndDate,
+                StartTimeInMinutes = acc.StartTimeInMinutes,
+                EndTimeInMinutes = acc.EndTimeInMinutes,
+                DayOfWeekNumber = acc.DayOfWeekNumber
+            }).OrderBy(x => x.DayOfWeekNumber);
+
+            var model = await PagingList<AccountDetail>.CreateAsync(test, 10, page);
+
+            return View(model);
         }
         public IActionResult CreateOneAccountDetailForCustomerAccount()
         {
